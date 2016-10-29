af4d69c7f9255848f2e69cd00412b17d92cbdc4a
Navjot Singh <weavebytes@gmail.com>
2016-10-29 14:33:34 +0530

    Adding compass to dashboard

--- examples/Dashboard/Dashboard.pro
+++ examples/Dashboard/Dashboard.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp\
+        ../../source/qcgaugewidget.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h\
+            ../../source/qcgaugewidget.h
 
 FORMS    += mainwindow.ui
--- examples/Dashboard/mainwindow.cpp
+++ examples/Dashboard/mainwindow.cpp
@@ -6,9 +6,61 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    compassGauge = new QcGaugeWidget;
+
+    compassGauge->addBackground(99);
+    QcBackgroundItem *bkg1 = compassGauge->addBackground(92);
+    bkg1->clearrColors();
+    bkg1->addColor(0.1,Qt::black);
+    bkg1->addColor(1.0,Qt::white);
+
+    QcBackgroundItem *bkg2 = compassGauge->addBackground(88);
+    bkg2->clearrColors();
+    bkg2->addColor(0.1,Qt::white);
+    bkg2->addColor(1.0,Qt::black);
+
+    QcLabelItem *w = compassGauge->addLabel(80);
+    w->setText("W");
+    w->setAngle(0);
+    w->setColor(Qt::white);
+
+    QcLabelItem *n = compassGauge->addLabel(80);
+    n->setText("N");
+    n->setAngle(90);
+    n->setColor(Qt::white);
+
+    QcLabelItem *e = compassGauge->addLabel(80);
+    e->setText("E");
+    e->setAngle(180);
+    e->setColor(Qt::white);
+
+    QcLabelItem *s = compassGauge->addLabel(80);
+    s->setText("S");
+    s->setAngle(270);
+    s->setColor(Qt::white);
+
+    QcDegreesItem *deg = compassGauge->addDegrees(70);
+    deg->setStep(5);
+    deg->setMaxDegree(270);
+    deg->setMinDegree(-75);
+    deg->setColor(Qt::white);
+    compassNeedle = compassGauge->addNeedle(60);
+    compassNeedle->setNeedle(QcNeedleItem::CompassNeedle);
+    compassNeedle->setValueRange(0,360);
+    compassNeedle->setMaxDegree(360);
+    compassNeedle->setMinDegree(0);
+    compassGauge->addBackground(7);
+    compassGauge->addGlass(88);
+    ui->verticalLayout->addWidget(compassGauge);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_horizontalSlider_valueChanged(int value)
+{
+    compassNeedle->setCurrentValue(value);
 }
